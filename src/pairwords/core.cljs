3bded5b9257ba61b5e9b4a51b0091aa068b8f28b
Alexander Solovyov <alexander@solovyov.net>
2012-12-18 00:29:21 +0200

    trying to build something useful

--- src/pairwords/core.cljs
+++ src/pairwords/core.cljs
@@ -3,6 +3,9 @@
   (:require [flapjax :as fj]
             [pairwords.util :refer [log]]))
 
+(defn prevent-default [es]
+  (fj/mapE #(.preventDefault %) es))
+
 (defn bind-timer []
   (fj/mapE log (fj/clicksE "start-timer")))
 
@@ -13,7 +16,20 @@
       (fj/insertValueE "timer" "innerHTML")
       ))
 
+;; event stream here works and behavior runs with initial value,
+;; but afterwards any events do not affect result. Why?
+(defn text-show-hide []
+  (-> (fj/clicksE "show")
+      (prevent-default)
+      (fj/collectE false (fn [_ val] (not val)))
+      (.mapE log)
+      (fj/startsWith false)
+      (fj/ifB "block" "none")
+      ;; could be "text" "style" "display" to change visibility
+      (fj/insertValueB "text" "data-display")))
+
 (defn ^:export run []
   (bind-timer)
   (bind-timer2)
+  (text-show-hide)
   (log "I'm running!"))
