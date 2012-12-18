daf117307af1ea6713848dae90ad64403ac1615d
Alexander Solovyov <alexander@solovyov.net>
2012-12-18 14:28:24 +0200

    show/hide working

--- src/pairwords/core.cljs
+++ src/pairwords/core.cljs
@@ -6,15 +6,6 @@
 (defn prevent-default [es]
   (fj/mapE #(.preventDefault %) es))
 
-(defn bind-timer []
-  (fj/mapE log (fj/clicksE "start-timer")))
-
-(defn bind-timer2 []
-  (-> "start-timer"
-      fj/clicksE
-      (.constantE 42)
-      (fj/insertValueE "timer" "innerHTML")
-      ))
 
 ;; event stream here works and behavior runs with initial value,
 ;; but afterwards any events do not affect result. Why?
@@ -22,14 +13,11 @@
   (-> (fj/clicksE "show")
       (prevent-default)
       (fj/collectE false (fn [_ val] (not val)))
-      (.mapE log)
       (fj/startsWith false)
       (fj/ifB "block" "none")
-      ;; could be "text" "style" "display" to change visibility
-      (fj/insertValueB "text" "data-display")))
+      ;; should be "text" "style" "display" to change visibility
+      (fj/insertValueB "text" "style" "display")))
 
 (defn ^:export run []
-  (bind-timer)
-  (bind-timer2)
   (text-show-hide)
   (log "I'm running!"))
