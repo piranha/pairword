3062df734ef84b01abb5ce60e809eaf7bdd507c9
Alexander Solovyov <alexander@solovyov.net>
2013-02-15 12:32:22 +0200

    update dependecies

--- project.clj
+++ project.clj
@@ -1,27 +1,22 @@
 (defproject pairwords "0.0.1"
   :description "Explaining words in pairs"
-  :plugins [[lein-cljsbuild "0.2.10" :hooks false]]
+  :plugins [[lein-cljsbuild "0.3.0" :hooks false]]
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [enfocus "1.0.0-beta2"]]
-  :cljsbuild {:builds
-              {
-               :main {
-                :source-path "src"
-                :compiler {
-                 :output-to "build/pairwords.js"
-                 :optimizations :whitespace
-                 :pretty-print true
-                 :foreign-libs [{:file "http://www.flapjax-lang.org/download/flapjax-2.1.js"
-                                 :provides "flapjax"}]}
-                }
-               :mini {
-                :source-path "src"
-                :compiler {
-                 :output-to "build/mini.js"
-                 :optimizations :advanced
-                 :externs ["externs/flapjax.js"]
-                 :foreign-libs [{:file "http://www.flapjax-lang.org/download/flapjax-2.1.js"
-                                 :provides "flapjax"}]}
-                }
-               }}
+                 [enfocus "1.0.0-beta3"]]
+  :cljsbuild
+  {:builds
+   [{:id "main"
+     :source-paths ["src"]
+     :compiler {:pretty-print true,
+                :output-to "build/pairwords.js",
+                :foreign-libs [{:provides "flapjax",
+                                :file "http://www.flapjax-lang.org/download/flapjax-2.1.js"}],
+                :optimizations :whitespace}}
+    {:id "mini"
+     :source-paths ["src"]
+     :compiler {:output-to "build/mini.js",
+                :foreign-libs [{:provides "flapjax",
+                                :file "http://www.flapjax-lang.org/download/flapjax-2.1.js"}],
+                :externs ["externs/flapjax.js"],
+                :optimizations :advanced}}]}
 )
